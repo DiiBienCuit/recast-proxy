18e1e5e23a32b0ca7301ebb386a71732e0cd8f2f
KPouianou <kosmas@visionmobile.com>
2018-05-22 17:27:17 +0300

    Implement basic end to end communication: recast -> proxy -> Hana

--- index.js
+++ index.js
@@ -1,27 +1,55 @@
 require('dotenv').config();
 
+const https = require('https');
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
+
+// special https agent - ignore unauthenticated requests
+// necessary to circumvent Hana not having SSL installed
+const agent = new https.Agent({
+  rejectUnauthorized: false
+});
 
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
-// Recast will send a post request to /errors to notify important errors
-// described in a json body
+// Recast will send a post request to /errors to notify
+// important errors described in a json body
 app.post('/errors', (req, res) => {
-  console.error(JSON.stringify(req.body, null, 2));
+  console.error(req.body);
   res.sendStatus(200);
 });
 
 app.post('/employees/manager', (req, res) => {
-  console.log(JSON.stringify(req.body, null, 2));
-  res.send(JSON.stringify(req.body, null, 2));
+  const name = _.chain(req.body)
+    .get('conversation.memory.name.value')
+    .capitalize()
+    .value();
+
+  const surname = _.chain(req.body)
+    .get('conversation.memory.surname.value')
+    .capitalize()
+    .value();
+
+  axios
+    .get(`${process.env.XSA_ENDPOINT}?name=${name}&surname=${surname}`, {
+      httpsAgent: agent
+    })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+      console.error('err');
+      res.send(`Error (${err.status}): ${err.statusText}`);
+    });
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(`App started on port ${process.env.PORT}`)
-);
+app.listen(process.env.PORT, () => {
+  console.log(`App started on port ${process.env.PORT}`);
+});
