44e3ee394c6f69208ca1dfcd0f661c245ddd5dd2
KPouianou <kosmas@visionmobile.com>
2018-05-22 12:57:07 +0300

    Basic Express integration

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+// Recast will send a post request to /errors to notify important errors
+// described in a json body
+app.post('/errors', (req, res) => {
+  console.error(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.post('/eployees/manager', (req, res) => {
+  res.send(JSON.stringify(req.body, null, 2));
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`App started on port ${process.env.PORT}`)
+);
